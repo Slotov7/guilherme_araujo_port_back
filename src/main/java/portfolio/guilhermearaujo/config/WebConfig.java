42f6532144f88799cbe949435615835856eef2a2
Guilherme Araújo <132411540+Slotov7@users.noreply.github.com>
2025-07-27 09:57:56 -0300

    fix: update CORS configuration to specify allowed origins

--- src/main/java/portfolio/guilhermearaujo/config/WebConfig.java
+++ src/main/java/portfolio/guilhermearaujo/config/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements  WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // 1. Aplica esta configuração a TODOS os endpoints da nossa API
-                .allowedOriginPatterns("*") // aceita coringas e protocolos diferentes
+                .allowedOrigins("https://guilherme-araujo.vercel.app", "http://localhost/3000") // aceita coringas e protocolos diferentes
                 .allowedMethods("*") // 3. Permite os métodos HTTP que usamos
                 .allowedHeaders("*") // 4. Permite todos os cabeçalhos nos pedidos
                 .allowCredentials(true); // 5. Permite o envio de credenciais (como cookies ou tokens de autorização)
