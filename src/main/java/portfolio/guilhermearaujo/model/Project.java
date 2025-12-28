99ed16bc95ad8ce0c5c1b95ff7088a63ab3d4140
Guilherme Araújo <132411540+Slotov7@users.noreply.github.com>
2025-12-28 11:06:46 -0300

    application

--- src/main/java/portfolio/guilhermearaujo/model/Project.java
+++ src/main/java/portfolio/guilhermearaujo/model/Project.java
@@ -3,20 +3,24 @@ package portfolio.guilhermearaujo.model;
 import jakarta.persistence.*;
 import lombok.Data;
 
-// Indica que esta classe é uma entidade do JPA e será mapeada para uma tabela no banco de dados
 @Entity
-// Gera automaticamente getters, setters, equals, hashCode e toString usando Lombok
 @Data
 public class Project {
-    // Define o atributo como chave primária
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    // Atributos do projeto
-    private String name; // Nome do projeto
-    private String description; // Descrição do projeto
-    private String imageUrl; // URL da imagem do projeto
-    private String repoUrl; // URL do repositório do projeto
+    private String name;
+
+    @Column(columnDefinition = "TEXT")
+    private String description;
+
+    @Column(length = 1000)
+    private String imageUrl;
+
+    @Column(nullable = true)
+    private String repoUrl;
+
+    @Column(length = 1000)
     private String technologies;
 }
